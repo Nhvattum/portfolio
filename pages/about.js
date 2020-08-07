baf46c72730d1a3ce92eac67e986068695121f1e
Nick Hvattum <nhvattum@gmail.com>
2020-08-07 16:16:12 -0700

    added random testing pages

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/navbar'
+
+export default function About() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Hi!  My name is Nick 😁</h1>
+        </div>
+    )
+}
