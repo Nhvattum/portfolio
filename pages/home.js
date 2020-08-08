48e16960c0384254ce35ee2f048a5dde997b54d7
Nick Hvattum <nhvattum@gmail.com>
2020-08-08 16:14:58 -0700

    started adding tehc logos to homepage

--- pages/home.js
+++ pages/home.js
@@ -2,6 +2,17 @@ export default function Home() {
     return (
         <div>
             <h1>Nick Hvattum</h1>
+            <h3>I am a Software Engineer motivated by utilizing my background in QA to achieve practical, precise solutions to technical problems. I enjoy using my unique background along with my technical skills to create software that will impact the life of the end user.
+            </h3>
+            <h2>Technologies</h2>
+            <div>
+                <ul>
+                    <li><img src='./public/css.png'></img></li>
+                </ul>
+                <div>Icons made by 
+                    <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+                </div>
+            </div>
         </div>
     )    
 }
