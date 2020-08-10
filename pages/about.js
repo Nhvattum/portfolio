5fb12bfd7e6ad483b530499d158986c76ea2888b
Nick Hvattum <nhvattum@gmail.com>
2020-08-09 23:29:09 -0700

    added a little info to about page

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,9 @@ export default function About() {
     return (
         <div>
             <Navbar />
-            <h1>Hi!  My name is Nick 😁</h1>
+            <h1>Hi!  I'm Nick</h1>
+            <h3>I am a software developer, a husband, and a dad.  </h3>
+            <h3>I am </h3>
         </div>
     )
 }
