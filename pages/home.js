a65fecadacc8ab38b96ee1c1abc00d54fffdf6be
Nick Hvattum <nhvattum@gmail.com>
2020-08-09 17:18:23 -0700

    updated logos

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,6 @@
-import {Button, Fade} from 'react-bootstrap'
-import { useState } from 'react'
+import {Button, Fade, Accordion, Card} from 'react-bootstrap'
 
 export default function Home() {
-    const [openSD, setOpenSD] = useState(false)
-    const [openMIU, setOpenMIU] = useState(false)
 
 
     return (
@@ -18,52 +15,70 @@ export default function Home() {
                 <img className="logo" src="/css.png" alt="CSS logo"></img>
                 <img className="logo" src="/react.png" alt="react logo"></img>
                 <img className="logo" src="/python.png" alt="python logo"></img>
+                <img className="logo" src="/typescript.png" alt="typescript logo"></img>
+                <img className="logo" src="/mongodb.png" alt="mongo db logo"></img>
+                <img className="logo" src="/postgresql.png" alt="postgresql logo"></img>
+                <img className="logo" src="/nodejs.png" alt="node js logo"></img>
+                <img className="logo" src="/heroku.png" alt="heroku logo"></img>
+                <img className="logo" src="/vscode.png" alt="visual studio code logo"></img>
                 <img className="logo" src="/github.png" alt="github logo"></img>
                 
-                <div>Icons made by 
-                    <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+                <div>Icons by 
+                    <a href="https://icons8.com/" target="_blank"> icons8</a>
                 </div>    
             </div>
             <div>
                 <h2>Recent Projects</h2>
                 <div>
-                    <Button
-                        onClick={() => setOpenSD(!openSD)}
-                        aria-controls="example-fade-text"
-                        aria-expanded={openSD}
-                    >
-                        Social Distancing
-                    </Button>
-                    <Fade in={openSD}>
-                    <div>
-                        <h3>
-                            <a href="https://nhvattum.github.io/Social-Distancing/">Social Distancing</a>
-                        </h3>
-                        <a href="https://nhvattum.github.io/Social-Distancing/">
-                            <img className="socialDistancing" src="/SocialDistancing.png" alt="social distancing game screenshot"></img>
-                        </a>
-                        <div className="projectDescription">Social Distancing is a dungeon crawler game created with HTML, CSS, and JavaScript.</div>
-                    </div>
-                    </Fade>
-                    
-                    <Button
-                        onClick={() => setOpenMIU(!openMIU)}
-                        aria-controls="example-fade-text"
-                        aria-expanded={openMIU}
-                    >
-                        Mix It Up
-                    </Button>
-                    <Fade in={openMIU}>
-                    <div>
-                        <h3>
-                            <a href="https://mix-it-up-nh.herokuapp.com">Mix it Up</a>
-                        </h3>
-                        <a href="https://mix-it-up-nh.herokuapp.com">
-                            <img className="mixItUp" src="/MixItUp.png" alt="mix it up cocktail app screenshot"></img>
-                        </a>
-                        <div className="projectDescription">Mix It Up is a cocktail app that was created with HTML, CSS, JavaScript, Express, and sequelize before being deployed to Heroku.<br></br><br></br>This web app allows users to search for and store cocktail recipes to their favorites list and create virtual pantries containing ingredients that they currently have. </div>
-                    </div>
-                    </Fade>
+                    <Accordion className="accordion">
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                    Stay Inside
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body className="projectDescription">
+                                    <a href="https://mix-it-up-nh.herokuapp.com" target="_blank">
+                                        <img className="mixItUp" src="/MixItUp.png" alt="mix it up cocktail app screenshot"></img>
+                                    </a>
+                                    Mix It Up is a cocktail app that was created with HTML, CSS, JavaScript, Express, and sequelize before being deployed to Heroku. This web app allows users to search for and store cocktail recipes to their favorites list and create virtual pantries containing ingredients that they currently have.
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                    Mix It Up
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body className="projectDescription">
+                                    <a href="https://mix-it-up-nh.herokuapp.com" target="_blank">
+                                        <img className="mixItUp" src="/MixItUp.png" alt="mix it up cocktail app screenshot"></img>
+                                    </a>
+                                    Mix It Up is a cocktail app that was created with HTML, CSS, JavaScript, Express, and sequelize before being deployed to Heroku. This web app allows users to search for and store cocktail recipes to their favorites list and create virtual pantries containing ingredients that they currently have.
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                    Social Distancing
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="1">
+                                <Card.Body className="projectDescription">
+                                    <a href="https://nhvattum.github.io/Social-Distancing/" target="_blank">
+                                        <img className="socialDistancing" src="/SocialDistancing.png" alt="social distancing game screenshot"/>
+                                    </a>
+                                    Social Distancing is a dungeon crawler game created with HTML, CSS, and JavaScript.
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        </Accordion>
+
+
                 </div>
             </div>
         </div>
