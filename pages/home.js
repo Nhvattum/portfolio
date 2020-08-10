92a42bd28c3f5af9e0e12435222d319ab9e4dc41
Nick Hvattum <nhvattum@gmail.com>
2020-08-09 23:56:44 -0700

    changed projects to cards

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,5 @@
 import Navbar from '../components/navbar'
-import {Button, Accordion, Card} from 'react-bootstrap'
+import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
 
 export default function Home() {
 
@@ -10,7 +10,7 @@ export default function Home() {
             <h1>Nick Hvattum</h1>
             <h3>I am a Software Engineer motivated by utilizing my background in QA to achieve practical, precise solutions to technical problems. I enjoy using my unique background along with my technical skills to create software that will impact the life of the end user.
             </h3>
-            <h2>Technologies</h2>
+            <h1>Technologies</h1>
             <div className="logoList">
                 <img className="logo" src="/html-5.png" alt="html-5 logo"></img>
                 <img className="logo" src="/javascript.png" alt="javascript logo"></img>
@@ -30,56 +30,59 @@ export default function Home() {
                 </div>    
             </div>
             <div>
-                <h2>Recent Projects</h2>
-                <div>
-                    <Accordion className="accordion">
-                        <Card>
-                            <Card.Header>
-                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                                    Stay Inside
-                                </Accordion.Toggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="0">
-                                <Card.Body className="projectDescription">
-                                    <a href="https://mix-it-up-nh.herokuapp.com" target="_blank">
-                                        <img className="mixItUp" src="/MixItUp.png" alt="mix it up cocktail app screenshot"></img>
-                                    </a>
-                                    Mix It Up is a cocktail app that was created with HTML, CSS, JavaScript, Express, and sequelize before being deployed to Heroku. This web app allows users to search for and store cocktail recipes to their favorites list and create virtual pantries containing ingredients that they currently have.
-                                </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                        <Card>
-                            <Card.Header>
-                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                                    Mix It Up
-                                </Accordion.Toggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="0">
-                                <Card.Body className="projectDescription">
-                                    <a href="https://mix-it-up-nh.herokuapp.com" target="_blank">
-                                        <img className="mixItUp" src="/MixItUp.png" alt="mix it up cocktail app screenshot"></img>
-                                    </a>
-                                    Mix It Up is a cocktail app that was created with HTML, CSS, JavaScript, Express, and sequelize before being deployed to Heroku. This web app allows users to search for and store cocktail recipes to their favorites list and create virtual pantries containing ingredients that they currently have.
-                                </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                        <Card>
-                            <Card.Header>
-                                <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                                    Social Distancing
-                                </Accordion.Toggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey="1">
-                                <Card.Body className="projectDescription">
-                                    <a href="https://nhvattum.github.io/Social-Distancing/" target="_blank">
-                                        <img className="socialDistancing" src="/SocialDistancing.png" alt="social distancing game screenshot"/>
-                                    </a>
-                                    Social Distancing is a dungeon crawler game created with HTML, CSS, and JavaScript.
-                                </Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                        </Accordion>
+                <h1>Recent Projects</h1>
+                <div className="recentProjects">
 
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Title>Stay Inside</Card.Title>
+                        <Card.Img variant="top" src="/MixItUp.png" alt="mix it up cocktail app screenshot" />
+                        <Card.Body>
+                            <Card.Text>
+                            Mix It Up is a cocktail app that was created with HTML, CSS, JavaScript, Express, and sequelize before being deployed to Heroku. This web app allows users to search for and store cocktail recipes to their favorites list and create virtual pantries containing ingredients that they currently have.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem>
+                                <Card.Body>Try it out 
+                                    <Card.Link href="https://mix-it-up-nh.herokuapp.com"> here</Card.Link>
+                                </Card.Body>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Title>Mix It Up</Card.Title>
+                        <Card.Img variant="top" src="/MixItUp.png" alt="mix it up cocktail app screenshot" />
+                        <Card.Body>
+                            <Card.Text>
+                            Mix It Up is a cocktail app that was created with HTML, CSS, JavaScript, Express, and sequelize before being deployed to Heroku. This web app allows users to search for and store cocktail recipes to their favorites list and create virtual pantries containing ingredients that they currently have.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem>
+                                <Card.Body>Try it out 
+                                    <Card.Link href="https://mix-it-up-nh.herokuapp.com"> here</Card.Link>
+                                </Card.Body>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Title>Social Distancing</Card.Title>
+                        <Card.Img variant="top" src="/SocialDistancing.png" alt="social distancing game screenshot" />
+                        <Card.Body>
+                            <Card.Text>
+                            Social Distancing is a dungeon crawler game created with HTML, CSS, and JavaScript.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem>
+                                <Card.Body>Try it out 
+                                    <Card.Link href="https://mix-it-up-nh.herokuapp.com"> here</Card.Link>
+                                </Card.Body>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
 
                 </div>
             </div>
