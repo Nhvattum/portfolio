9e575e559176ac9712d93af3569b5cd2e1bacb24
Nick Hvattum <nhvattum@gmail.com>
2020-08-24 00:59:47 -0700

    added content to about page

--- pages/about.js
+++ pages/about.js
@@ -9,6 +9,8 @@ export default function About() {
             <h3>I am passionate about helping people, and I love being able to use my talents to improve the lives of those around me.</h3>
             <h3>I have a background in the medical field where I was going to school to be a nurse until an injury made that career path not possible anymore.</h3>
             <h3>More recently, I was a quality assurance technician at an aerospace manufaturing company.</h3>
+            <h3>I have always had an eye for finding errors. Whether it's a movie scene with a minor plot hole, or an article with the wrong their/there/they're, I am quick to point out these kinds of things.  You might read this and think that I'm a glass half empty person, only seeing the bad in things...but I am the opposite. I see every error as an opportunity to make things better. </h3>
+            <h3>After taking a few free coding courses online, I knew I was hooked and had to learn more. Coding is the perfect balance between problem solving when bugs occur, and being creative and inventive.  I have grown very passionate about it, and I am excited to use this new skill to impact the lives of others.  </h3>
         </div>
     )
 }
