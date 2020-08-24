34ad9a9cfc3ff4014f66db6dbd46054ee17f0a4b
Nick Hvattum <nhvattum@gmail.com>
2020-08-24 11:45:20 -0700

    fixing bootstrap

--- components/navbar.js
+++ components/navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
                 <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400&display=swap" rel="stylesheet"/>
                 <title>Nick Hvattum Portfolio</title>
                 <link rel="icon" href="/favicon.ico" />
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></link>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"></link>
             </Head>
             
             <Nav className="navBar "activeKey="/home">
