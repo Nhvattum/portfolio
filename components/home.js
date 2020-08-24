3bd8bbe6637f516bce448054b5110b6759d806a0
Nick Hvattum <nhvattum@gmail.com>
2020-08-24 04:18:46 -0700

    fixed bootstrap issue

--- components/home.js
+++ components/home.js
@@ -1,10 +1,19 @@
 import Navbar from './navbar'
 import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
 import Footer from './footer'
+import Head from 'next/head'
 
 export default function Home() {
     return (
         <div className="home">
+            <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400&display=swap" rel="stylesheet"/>
+                <title>Nick Hvattum Portfolio</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/konpa/devicon@master/devicon.min.css"></link>
+                {/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link> */}
+
+            </Head>
             
             <Navbar />
             <br/>
--- pages/resume.js
+++ pages/resume.js
@@ -1,9 +1,18 @@
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+import Head from 'next/head'
 
 export default function Resume() {
     return(
         <div className="home">
+            <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400&display=swap" rel="stylesheet"/>
+                <title>Nick Hvattum Portfolio</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/konpa/devicon@master/devicon.min.css"></link>
+                {/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link> */}
+
+            </Head>
             <Navbar />
             <img className="resume" src="/resume1.png" />
             <img className="resume" src="/resume2.png" />
