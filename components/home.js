ea24a6cfaaf6c9f5fa19c17d9f08d44e9fc881ed
Nick Hvattum <nhvattum@gmail.com>
2020-08-24 13:18:38 -0700

    fixing bootstrap

--- components/home.js
+++ components/home.js
@@ -1,11 +1,21 @@
 import Navbar from './navbar'
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
+import {ListGroup, ListGroupItem} from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
 import Footer from './footer'
+import Head from 'next/head'
 
 export default function Home() {
-
+    
     return (
         <div className="home">
+            <Head>
+            <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                crossorigin="anonymous"
+            />
+            </Head>
             <Navbar />
             <br/>
             
