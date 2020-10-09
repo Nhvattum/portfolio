92611c0e146f57229830e04c25a42e7035d76088
Nick Hvattum <nhvattum@gmail.com>
2020-10-09 15:45:26 -0700

    updated resume with Whitfields Auto Licensing

--- pages/resume.js
+++ pages/resume.js
@@ -1,14 +1,14 @@
-import Navbar from '../components/navbar'
-import Footer from '../components/footer'
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 export default function Resume() {
-    return(
-        <div className="resumeDiv">
-            <Navbar />
-            <img className="resume" src="/resumePage1.png" />
-            <img className="resume" src="/resumePage2.png" />
-            {/* <object className="pdf" data="/Nick Hvattum Resume.pdf#view=FitH"/> */}
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="resumeDiv">
+      <Navbar />
+      <img className="resume" src="/newResumePage1.png" />
+      <img className="resume" src="/newResumePage2.png" />
+      {/* <object className="pdf" data="/Nick Hvattum Resume.pdf#view=FitH"/> */}
+      <Footer />
+    </div>
+  );
 }
